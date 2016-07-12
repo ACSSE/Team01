2111a06f498bc63137eb345bb3729c9520929500
Casper Ndlovu <casperndlovu42@gmail.com>
2016-07-12 19:35:56 +0200

    WCF Services moved to proper directory

--- IceBreak/IceBreak/ITestService.cs
+++ IceBreak/IceBreak/ITestService.cs
@@ -5,7 +5,7 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
 
-namespace IcebreakServices
+namespace IceBreak
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "ITestService" in both code and config file together.
     [ServiceContract]
